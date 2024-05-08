a104ba84843d7ec858c1066bc79080be6318b489
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-08 18:11:17 +0000

    if it aint broke

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const numberInput = parseInt(document.getElementById("number-input").value)
+  let answer = 0
+
+  for (let counter = 0; counter < numberInput; counter++) {
+    answer += numberInput + 1
+  }
+
+  document.getElementById("answer").innerHTML = answer /2
+}
