ab7d89deef38d35eccf3d739b4e4caae65300eb5
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-08 18:37:51 +0000

    added storyboard + flowchart

--- js/script.js
+++ js/script.js
@@ -12,5 +12,5 @@ function myButtonClicked() {
     answer += numberInput + 1
   }
 
-  document.getElementById("answer").innerHTML = answer / 2
+  document.getElementById("answer").innerHTML = "The sum of all the numbers from 1-" + numberInput + " = " + answer / 2
 }
